3d72e63a39cac0b81a225f8265a33b33c9207b8b
Anish Gangu <56240492+AG-Coding@users.noreply.github.com>
2022-10-25 20:51:32 -0700

    auto retuned

--- src/main/java/frc/robot/commands/AutoCommandFactory.java
+++ src/main/java/frc/robot/commands/AutoCommandFactory.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IndexerSubsystem;
@@ -47,30 +48,34 @@ public class AutoCommandFactory {
   public SequentialCommandGroup fiveBallAuto() {
 
     return new SequentialCommandGroup(
-        new ParallelDeadlineGroup(
+       // new ParallelDeadlineGroup(
             autoPath("FiveBall1", true),
+            new WaitCommand(1),
             //new RunCommand(intaker::intakeBall, intaker)),
       //  new ShooterTeleop(shooter, indexer, vision, drive),
 
        // new ParallelDeadlineGroup(
             autoPath("FiveBall2", false),
+            new WaitCommand(1),
          //   new RunCommand(intaker::intakeBall, intaker)),
 
         autoPath("FiveBall3", false),
+        new WaitCommand(5),
        // new ShooterTeleop(shooter, indexer, vision, drive),
 
       //  new ParallelDeadlineGroup(
             autoPath("FiveBall4", false),
+            new WaitCommand(1),
         //    new RunCommand(intaker::intakeBall, intaker)),
 
-        autoPath("FiveBall5", false)));
+        autoPath("FiveBall5", false));
       //  new ShooterTeleop(shooter, indexer, vision, drive));
   }
 
   // Tuning/Debugging New Auto Paths
 
   public Command testAuto() {
-    return autoPath("FiveBall1", true);
+    return autoPath("FiveBall5", true);
   }
 
   public Command TuningThetaController180() {
